c8ff562fd929400ef1fb55f8cde4444b6d06c972
Pankaj Gaurav <pankajgaurav17@gmail.com>
2023-10-24 13:10:28 +0530

    Create subject.ts

---
+++ rxjs/subject.ts
@@ -0,0 +1,6 @@
+Subject
+What is a Subject? An RxJS Subject is a special type of Observable that allows values to be multicasted to many Observers. While plain Observables are unicast (each subscribed Observer owns an independent execution of the Observable), Subjects are multicast.
+
+A Subject is like an Observable, but can multicast to many Observers. Subjects are like EventEmitters: they maintain a registry of many listeners.
+
+  https://rxjs.dev/guide/subject
